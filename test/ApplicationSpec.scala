930af0d99846d2143458e42c1549cee750976d75
Ruffy <johan.ellfries@gmail.com>
2016-11-20 18:02:57 +0100

    Upgraded to play 2.5

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,27 +1,36 @@
 import org.specs2.mutable._
-import org.specs2.runner._
-import org.junit.runner._
-
-import play.api.test._
+import org.specs2.specification.BeforeAll
+import play.api.{Application, Mode}
+import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test.Helpers._
+import play.api.test._
 
 /**
  * Add your spec here.
  * You can mock out a whole application including requests, plugins etc.
  * For more information, consult the wiki.
  */
-@RunWith(classOf[JUnitRunner])
-class ApplicationSpec extends Specification {
+class ApplicationSpec extends Specification with BeforeAll {
 
-  val fakeApplication = new FakeApplication()
+//  val fakeApplication = new FakeApplication()
+  var fakeApplication: Application = _
+
+  override def beforeAll(): Unit = {
+   fakeApplication = new GuiceApplicationBuilder()
+//      .in(new File("path/to/app"))
+      .in(Mode.Test)
+//      .in(classLoader)
+      .build
+  }
+
   "Application" should {
 
-    "send 404 on a bad request" in new WithApplication(fakeApplication){
-      route(FakeRequest(GET, "/boum")) must beSome.which (status(_) == NOT_FOUND)
+    "send 404 on a bad request" in {//in new WithApplication(fakeApplication){
+      route(fakeApplication, FakeRequest(GET, "/boum")) must beSome.which (status(_) == NOT_FOUND)
     }
 
-    "render the index page" in new WithApplication(fakeApplication){
-      val home = route(FakeRequest(GET, "/")).get
+    "render the index page" in { //new WithApplication(fakeApplication){
+      val home = route(fakeApplication, FakeRequest(GET, "/")).get
 
       status(home) must equalTo(OK)
       contentType(home) must beSome.which(_ == "text/html")
