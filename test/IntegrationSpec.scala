476b990ec43a5db7cbd42a40f232848abde0ac16
Ruffy <johan.ellfries@gmail.com>
2016-11-22 21:36:13 +0100

    Fixed integration test.

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -1,27 +1,17 @@
-import org.openqa.selenium.WebDriver
-import org.openqa.selenium.chrome.ChromeDriver
 import org.scalatest.BeforeAndAfterAll
-import org.scalatestplus.play.{OneAppPerSuite, PlaySpec}
-import play.test.WithBrowser
+import org.scalatestplus.play._
 
 /**
  * add your integration spec here.
  * An integration test will fire up a whole play application in a real (or headless) browser
  */
-class IntegrationSpec extends PlaySpec with BeforeAndAfterAll with OneAppPerSuite {
-
-  var driver : WebDriver = _
-
-  override def beforeAll(): Unit = {
-    driver =  new ChromeDriver()
-  }
+class IntegrationSpec extends PlaySpec with BeforeAndAfterAll with OneServerPerSuite with ChromeFactory with OneBrowserPerSuite {
 
   "Application" should {
 
-    "work from within a browser" in new WithBrowser() {
-      browser.goTo("http://localhost:" + port)
-
-      browser.pageSource must contain("Your new application is ready.")
+    "work from within a browser" in {
+      go to "http://localhost:" + port
+      assert(pageSource.contains("Your new application is ready."))
     }
   }
 }
