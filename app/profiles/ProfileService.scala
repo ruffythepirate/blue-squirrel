b6d6103ad85ba45b0be87d3d8ff464b47038579c
Ruffy <johan.ellfries@gmail.com>
2017-02-07 19:35:12 +0100

    Updated look of profile to the left.

--- app/profiles/ProfileService.scala
+++ app/profiles/ProfileService.scala
@@ -7,7 +7,7 @@ class ProfileService() {
   def getProfile: Profile = {
 
     Profile(
-      Person("Johan", ""),
+      Person("Johan", "http://www.aspirehire.co.uk/aspirehire-co-uk/_img/profile.svg"),
       Position("Fullstack Developer", "IS24", ""),
       Seq.empty[SocialLink])
   }
--- app/profiles/model/Profile.scala
+++ app/profiles/model/Profile.scala
@@ -2,7 +2,7 @@ package profiles.model
 
 case class Profile(person: Person, currentPosition: Position, socialLinks: Seq[SocialLink])
 
-case class Person(name: String, shortBiography: String)
+case class Person(name: String, imageUrl:String = "", shortBiography: String = "")
 
 case class Position(title: String, company: String, url: String)
 
