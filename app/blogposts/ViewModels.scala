878f281d40deb97b943e00c00fb0f1b2fb9c636e
Ruffy <johan.ellfries@gmail.com>
2017-02-07 12:16:14 +0100

    Changed style of front page entries list.

--- app/blogposts/ViewModels.scala
+++ app/blogposts/ViewModels.scala
@@ -1,14 +1,15 @@
 package blogposts
 
 import play.api.libs.json.Json
+import org.joda.time.DateTime
 
 
-case class BlogPostViewModel(id: Option[Long], title: String, body: String, tags: Seq[String])
+case class BlogPostViewModel(id: Option[Long], title: String, body: String, tags: Seq[String], createdDate: Option[DateTime])
 
 object BlogPostViewModel {
   implicit val blogPostFormat = Json.format[BlogPostViewModel]
 
   def apply(blogPost: BlogPost): BlogPostViewModel = {
-    BlogPostViewModel(Some(blogPost.id), blogPost.title, blogPost.body, Seq.empty)
+    BlogPostViewModel(Some(blogPost.id), blogPost.title, blogPost.body, Seq.empty, blogPost.createdDate)
   }
 }
